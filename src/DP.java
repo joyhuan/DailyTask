8ae31bc2d83a79dc4ce41d193cdac2cefa8e1f65
joyhuan <joy_huanqi@outlook.com>
2020-08-22 22:26:54 -0500

    interleaving String

--- src/DP.java
+++ src/DP.java
@@ -12,6 +12,36 @@ public class DP {
         return num;
     }
 
+
+
+    /**
+     * 97. Interleaving String
+     * 肯定得DP 不能DFS啊 之前还是直觉不够灵敏
+     * @param s1
+     * @param s2
+     * @param s3
+     * @return
+     */
+    public boolean isInterleave(String s1, String s2, String s3) {
+        if(s3.length() != s1.length() + s2.length())
+            return false;
+
+        boolean[][] table= new boolean[s1.length()+1][s2.length()+1];
+
+        for(int i=0; i<s1.length()+1; i++)
+            for(int j=0; j< s2.length()+1; j++){
+                if(i==0 && j==0)
+                    table[i][j] = true;
+                else if(i == 0)
+                    table[i][j] = ( table[i][j-1] && s2.charAt(j-1) == s3.charAt(i+j-1));
+                else if(j == 0)
+                    table[i][j] = ( table[i-1][j] && s1.charAt(i-1) == s3.charAt(i+j-1));
+                else
+                    table[i][j] = (table[i-1][j] && s1.charAt(i-1) == s3.charAt(i+j-1) ) || (table[i][j-1] && s2.charAt(j-1) == s3.charAt(i+j-1) );
+            }
+        return table[s1.length()][s2.length()];
+    }
+    
     /**
      * 10. Regular Expression Matching
      * '.' Matches any single character.
